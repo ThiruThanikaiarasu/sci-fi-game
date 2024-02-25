eff3faba1b7300f7465729f0367dcaa0ea78bc69
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-02-25 23:52:33 +0530

    ADD. Included the start, restart and full screen logics.

--- assets/JS/enemy.js
+++ assets/JS/enemy.js
@@ -1,62 +1,58 @@
 class Enemy {
-    constructor(game) {
-        this.game = game
-        this.x 
-        this.y 
-        // this.y = Math.random() * this.game.height
-        this.speedX = 0 
-        this.speedY = Math.random() * 2 + 0.2
-        this.width = 50
-        this.height = 50
-        this.lives 
-        this.free = true
-    }
-    isAlive(){
-        return this.lives >= 1
+    constructor(game){
+        this.game = game;
+        this.width = 50;
+        this.height = 50;
+        this.x;
+        this.y;
+        this.speedX = 0;
+        this.speedY = Math.random() * 2 + 0.2;
+        this.lives;
+        this.free = true;
     }
     start(){
-        this.x = Math.random() * this.game.width
-        this.y = -this.height
-        this.lives = 1
-        this.free = false
+        this.x = Math.random() * this.game.width;
+        this.y = -this.height;
+        this.lives = 2;
+        this.free = false;
     }
     reset(){
-        this.free = true
+        this.free = true;
+    }
+    isAlive(){
+        return this.lives >= 1;
     }
     update(){
-        if(!this.free){
-            // float in 
-            if (this.y < 0) this.y += 5
+        if (!this.free){
+            // float in
+            if (this.y < 0) this.y += 5;
             // make sure always visible
             if (this.x > this.game.width - this.width){
-                this.x = this.game.width - this.width
+                this.x = this.game.width - this.width;
             }
-            this.x += this.speedX
-            this.y += this.speedY
 
-            // check Collision 
-            if (this.game.checkCollision(this, this.game.mouse) && this.game.mouse.pressed) {
+            this.x += this.speedX;
+            this.y += this.speedY;
+
+            // check collision
+            if (this.game.checkCollision(this, this.game.mouse) && this.game.mouse.pressed && !this.game.mouse.fired){
                 this.lives--;
+                this.game.mouse.fired = true;
             }
-    
-            // Remove enemy if lives are zero
-            if (!this.isAlive()) {
+            if (!this.isAlive()){
                 this.reset();
-                return; // Exit update function to prevent further processing
+                this.game.score++;
             }
-
-            if(this.y > this.game.height){
-                this.reset() 
+            if (this.y > this.game.height){
+                this.reset();
+                this.game.lives--;
             }
         }
-        
     }
-    draw(){ // custom draw method create enemy 
-        if(!this.free){
-            this.game.ctx.fillStyle = 'red'
-            this.game.ctx.fillRect(this.x, this.y, this.width, this.height)
-            this.game.ctx.fillStyle = 'blue'
-            this.game.ctx.fillText(this.lives, this.x + this.width * 0.5, this.y + this.height * 0.5)
+    draw(){
+        if (!this.free){
+             this.game.ctx.strokeRect(this.x, this.y, this.width, this.height);
+            this.game.ctx.fillText(this.lives, this.x + this.width * 0.5, this.y + this.height * 0.5);
         }
     }
 }
